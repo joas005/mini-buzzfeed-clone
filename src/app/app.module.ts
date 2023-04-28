8454b77d735f55afb8bb70c7ff007424324a4ad5
joas <joaoferreirapaula005@gmail.com>
2023-04-28 10:38:28 -0300

    Melhorando a complexidade do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuizzComponent } from './components/quizz/quizz.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BtnComponent } from './components/btn/btn.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuizzComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    BtnComponent
   ],
   imports: [
     BrowserModule,
